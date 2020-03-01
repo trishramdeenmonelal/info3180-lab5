f1d60b38ab5798f1d854effe6144b863519ceda7
trishramdeenmonelal <shawn.ramdeen16@gmail.com>
2020-03-01 10:07:27 -0600

    Ex9 Completed :)

--- app/views.py
+++ app/views.py
@@ -26,6 +26,13 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html')
+    
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    """Render the website's secure page."""
+    return render_template('secure_page.html')
+
 
 
 @app.route("/login", methods=["GET", "POST"])
